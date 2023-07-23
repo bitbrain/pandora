a4b636333433cef108232d46176161183d89958f
miguel <miguel-gonzalez@gmx.de>
2023-07-23 15:20:08 +0100

    🔌 Reference type (#38)

---
+++ addons/pandora/ui/components/properties/reference/reference_property.gd
@@ -0,0 +1,23 @@
+@tool
+extends PandoraPropertyControl
+
+
+@onready var entity_picker = $EntityPicker
+
+
+func _ready() -> void:
+	refresh()
+	entity_picker.entity_selected.connect(
+		func(entity:PandoraEntity):
+			var reference = PandoraReference.new(entity.get_entity_id())
+			_property.set_default_value(reference)
+			property_value_changed.emit())
+
+
+func refresh() -> void:
+	if _property != null:
+		var default_value = _property.get_default_value() as PandoraReference
+		if default_value != null:
+			var entity = default_value.get_entity()
+			if entity != null:
+				entity_picker.select(entity)
