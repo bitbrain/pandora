0efec8b0313f8a412c2525666ffae2429797b8f3
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-07-23 16:24:29 +0100

    :sparkles: generate entity ids

--- TestScene.gd
+++ TestScene.gd
@@ -8,3 +8,5 @@ extends CenterContainer
 func _ready() -> void:
 	var instance = Pandora.create_entity_instance(entity)
 	print(Pandora.serialize_entity_instance(instance))
+	
+	var entity = Pandora.get_entity(EntityIds.NEW_ENTITY)
---
+++ pandora/entity_ids.gd
@@ -0,0 +1,9 @@
+# Do not modify! Auto-generated file.
+class_name EntityIds
+
+
+const NEW_ENTITY = "5"
+const NEW_ENTITY1 = "6"
+const NEW_ENTITY2 = "7"
+const NEW_ENTITY3 = "8"
+const NEW_ENTITY4 = "9"
