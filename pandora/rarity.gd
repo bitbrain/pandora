4f7a46a743f445cbb676a8a7f12c4f3d94df10c3
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-07-29 14:52:57 +0100

    setup example

---
+++ pandora/rarity.gd
@@ -0,0 +1,8 @@
+# Do not modify! Auto-generated file.
+class_name Rarity 
+
+
+const COMMON = "17"
+const RARE = "18"
+const EPIC = "19"
+const LEGENDARY = "20"
---
+++ pandora/tools.gd
@@ -0,0 +1,5 @@
+# Do not modify! Auto-generated file.
+class_name Tools 
+
+
+const PICKAXE = "6"
