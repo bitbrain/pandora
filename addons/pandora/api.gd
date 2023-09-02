3f64b15d35b609640b68c53d8fc94b1595703adf
Elliot Thomas <eth0net@pm.me>
2023-09-02 22:51:37 +0100

    reformat api

--- addons/pandora/api.gd
+++ addons/pandora/api.gd
@@ -2,25 +2,23 @@
 extends Node
 
 
-const EntityIdFileGenerator = preload("res://addons/pandora/util/entity_id_file_generator.gd")
-
-
 signal data_loaded
-signal entity_added(entity:PandoraEntity)
+signal entity_added(entity: PandoraEntity)
 
 
-var _context_manager:PandoraContextManager
-var _storage:PandoraJsonDataStorage
-var _id_generator:NanoIDGenerator
-var _entity_backend:PandoraEntityBackend
+const EntityIdFileGenerator := preload("res://addons/pandora/util/entity_id_file_generator.gd")
 
 
+var _context_manager: PandoraContextManager
+var _storage: PandoraJsonDataStorage
+var _id_generator: NanoIDGenerator
+var _entity_backend: PandoraEntityBackend
 var _loaded = false
 
 
 func _enter_tree() -> void:
-	self._storage = PandoraJsonDataStorage.new("res://")
 	self._context_manager = PandoraContextManager.new()
+	self._storage = PandoraJsonDataStorage.new("res://")
 	self._id_generator = NanoIDGenerator.new(NanoIDAlphabets.URL, 10)
 	self._entity_backend = PandoraEntityBackend.new(_id_generator)
 	self._entity_backend.entity_added.connect(func(entity): entity_added.emit(entity))
@@ -29,48 +27,49 @@ func _enter_tree() -> void:
 
 func _exit_tree() -> void:
 	_clear()
-	_entity_backend = null
 	_context_manager = null
-	_id_generator = null
 	_storage = null
+	_id_generator = null
+	_entity_backend = null
+
 
 func get_context_id() -> String:
 	return _context_manager.get_context_id()
 
 
-func set_context_id(context_id:String) -> void:
+func set_context_id(context_id: String) -> void:
 	_context_manager.set_context_id(context_id)
 
 
-func create_entity(name:String, category:PandoraCategory) -> PandoraEntity:
-	return _entity_backend.create_entity(name, category)
-
-
-func create_category(name:String, parent_category:PandoraCategory = null) -> PandoraCategory:
+func create_category(name: String, parent_category: PandoraCategory = null) -> PandoraCategory:
 	return _entity_backend.create_category(name, parent_category)
 
 
-func create_property(on_category:PandoraCategory, name:String, type:String) -> PandoraProperty:
+func create_entity(name: String, category: PandoraCategory) -> PandoraEntity:
+	return _entity_backend.create_entity(name, category)
+
+
+func create_property(on_category: PandoraCategory, name: String, type: String) -> PandoraProperty:
 	return _entity_backend.create_property(on_category, name, type)
 
 
-func delete_category(category:PandoraCategory) -> void:
+func delete_category(category: PandoraCategory) -> void:
 	_entity_backend.delete_category(category)
 
 
-func delete_entity(entity:PandoraEntity) -> void:
+func delete_entity(entity: PandoraEntity) -> void:
 	_entity_backend.delete_entity(entity)
 
 
-func get_entity(entity_id:String) -> PandoraEntity:
+func get_entity(entity_id: String) -> PandoraEntity:
 	return _entity_backend.get_entity(entity_id)
 
 
-func get_category(category_id:String) -> PandoraCategory:
+func get_category(category_id: String) -> PandoraCategory:
 	return _entity_backend.get_category(category_id)
 
 
-func get_property(property_id:String) -> PandoraProperty:
+func get_property(property_id: String) -> PandoraProperty:
 	return _entity_backend.get_property(property_id)
 
 
@@ -78,18 +77,21 @@ func get_all_roots() -> Array[PandoraCategory]:
 	return _entity_backend.get_all_roots()
 
 
-func get_all_categories(filter:PandoraCategory = null, sort:Callable = func(a,b): return false) -> Array[PandoraEntity]:
+func get_all_categories(filter: PandoraCategory = null, sort: Callable = func(a,b): return false) -> Array[PandoraEntity]:
 	return _entity_backend.get_all_categories(filter, sort)
 
 
-func get_all_entities(filter:PandoraCategory = null, sort:Callable = func(a,b): return false) -> Array[PandoraEntity]:
+func get_all_entities(filter: PandoraCategory = null, sort: Callable = func(a,b): return false) -> Array[PandoraEntity]:
 	return _entity_backend.get_all_entities(filter, sort)
 
 
-func load_data_async() -> void:
-	var thread = Thread.new()
-	if thread.start(load_data) != 0:
-		push_error("Unable to load Pandora data in async mode.")
+func save_data() -> void:
+	var all_object_data = {
+			"_entity_data": _entity_backend.save_data()
+		}
+	_storage.store_all_data(all_object_data, _context_manager.get_context_id())
+
+	EntityIdFileGenerator.regenerate_id_files(get_all_roots())
 
 
 func load_data() -> void:
@@ -104,13 +106,10 @@ func load_data() -> void:
 	data_loaded.emit()
 
 
-func save_data() -> void:
-	var all_object_data = {
-			"_entity_data": _entity_backend.save_data()
-		}
-	_storage.store_all_data(all_object_data, _context_manager.get_context_id())
-
-	EntityIdFileGenerator.regenerate_id_files(get_all_roots())
+func load_data_async() -> void:
+	var thread = Thread.new()
+	if thread.start(load_data) != 0:
+		push_error("Unable to load Pandora data in async mode.")
 
 
 func is_loaded() -> bool:
@@ -121,7 +120,7 @@ func serialize(instance:PandoraEntityInstance) -> Dictionary:
 	return instance.save_data()
 
 
-func deserialize(data:Dictionary) -> PandoraEntityInstance:
+func deserialize(data: Dictionary) -> PandoraEntityInstance:
 	if not data.has("_entity_id"):
 		push_error("Unable to deserialize data! Invalid PandoraEntityInstance format.")
 		return
