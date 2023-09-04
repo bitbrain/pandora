fbbfec0c81c4cb966d57ef7df62e5debd30f3a42
Felipe Martins <mrfelipemartins@gmail.com>
2023-09-04 17:38:17 -0300

    style(data): lint code

--- addons/pandora/storage/json/json_data_storage.gd
+++ addons/pandora/storage/json/json_data_storage.gd
@@ -21,7 +21,7 @@ func _init(data_dir: String):
 
 func store_all_data(data:Dictionary, context_id: String) -> Dictionary:
 	var file_path = _get_file_path(context_id)
-	var file : FileAccess
+	var file: FileAccess
 	if OS.is_debug_build():
 		file = FileAccess.open(file_path, FileAccess.WRITE)
 		file.store_string(JSON.stringify(data, "\t"))
@@ -34,12 +34,12 @@ func store_all_data(data:Dictionary, context_id: String) -> Dictionary:
 
 func get_all_data(context_id: String) -> Dictionary:
 	var file_path = _get_file_path(context_id)
-	var file : FileAccess
+	var file: FileAccess
 	if OS.is_debug_build():
 		file = FileAccess.open(file_path, FileAccess.READ)
 	else:
 		file = FileAccess.open_compressed(file_path, FileAccess.READ)
-	var json : JSON = JSON.new()
+	var json: JSON = JSON.new()
 	if file != null:
 		var text = file.get_as_text()
 		json.parse(text)
@@ -53,11 +53,11 @@ func get_all_data(context_id: String) -> Dictionary:
 		return {}
 
 func get_decompressed_data(file_path : String) -> Dictionary:
-	var file : FileAccess = FileAccess.open_compressed(file_path, FileAccess.READ)
+	var file: FileAccess = FileAccess.open_compressed(file_path, FileAccess.READ)
 	if file != null:
 		var text = file.get_as_text()
 		file.close()
-		var json : JSON = JSON.new()
+		var json: JSON = JSON.new()
 		json.parse(text)
 		return json.get_data() as Dictionary
 	else:
@@ -80,7 +80,7 @@ func _get_file_path(context_id: String) -> String:
 
 
 func _load_from_file(file_path: String) -> Dictionary:
-	var file : FileAccess
+	var file: FileAccess
 	if OS.is_debug_build():
 		file = FileAccess.open(file_path, FileAccess.READ)
 	else:
