6576124d970acd89abe4ad6e57a178fdd8e27bd6
Felipe Martins <mrfelipemartins@gmail.com>
2023-09-05 21:09:25 -0300

    refactor(drag-and-drop) removing debug dumps

--- addons/pandora/ui/editor/property_editor/property_editor.gd
+++ addons/pandora/ui/editor/property_editor/property_editor.gd
@@ -35,7 +35,6 @@ func edit_key(property_name:String) -> void:
 
 
 func set_entity(entity:PandoraEntity) -> void:
-	print("Setting entity to " + str(entity))
 	property_settings_container.visible = entity is PandoraCategory
 	property_settings_container.set_property(null)
 	for child in property_list.get_children():
