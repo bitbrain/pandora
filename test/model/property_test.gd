ce2daab1b57252015c188a2da37633f385277651
Nathan Krischer <me@nathankrischer.com>
2023-11-03 20:44:19 -0400

    added unit tests for vector properties

--- test/model/property_test.gd
+++ test/model/property_test.gd
@@ -15,80 +15,80 @@ func test_string_property() -> void:
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property).is_equal(property)
-	
-	
+
+
 func test_string_property_wrong_type() -> void:
 	var property = PandoraProperty.new("123", "property", "reference")
 	property.set_default_value("Hello World")
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property.get_default_value()).is_null()
-	
-	
+
+
 func test_int_property() -> void:
 	var property = PandoraProperty.new("123", "property", "int")
 	property.set_default_value(123)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property).is_equal(property)
-	
-	
+
+
 func test_int_property_wrong_type() -> void:
 	var property = PandoraProperty.new("123", "property", "string")
 	property.set_default_value(123)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property.get_default_value()).is_equal("")
-	
-	
+
+
 func test_bool_property() -> void:
 	var property = PandoraProperty.new("123", "property", "bool")
 	property.set_default_value(true)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property).is_equal(property)
-	
-	
+
+
 func test_bool_property_wrong_type() -> void:
 	var property = PandoraProperty.new("123", "property", "float")
 	property.set_default_value(false)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property.get_default_value()).is_equal(0.0)
-	
-	
+
+
 func test_float_property() -> void:
 	var property = PandoraProperty.new("123", "property", "float")
 	property.set_default_value(10.3)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property).is_equal(property)
-	
-	
+
+
 func test_float_property_wrong_type() -> void:
 	var property = PandoraProperty.new("123", "property", "int")
 	property.set_default_value(1.23)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property.get_default_value()).is_equal(0)
-	
-	
+
+
 func test_color_property() -> void:
 	var property = PandoraProperty.new("123", "property", "color")
 	property.set_default_value(Color.RED)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property).is_equal(property)
-	
-	
+
+
 func test_color_property_wrong_type() -> void:
 	var property = PandoraProperty.new("123", "property", "reference")
 	property.set_default_value(Color.RED)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property.get_default_value()).is_null()
-	
-	
+
+
 func test_reference_property() -> void:
 	var ref = PandoraReference.new("12345", 0)
 	var property = PandoraProperty.new("123", "property", "reference")
@@ -96,8 +96,8 @@ func test_reference_property() -> void:
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property).is_equal(property)
-	
-	
+
+
 func test_reference_property_wrong_type() -> void:
 	var ref = PandoraReference.new("12345", 0)
 	var property = PandoraProperty.new("123", "property", "bool")
@@ -105,16 +105,16 @@ func test_reference_property_wrong_type() -> void:
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_bool(new_property.get_default_value()).is_false()
-	
-	
+
+
 func test_unknown_type() -> void:
 	var property = PandoraProperty.new("123", "property", "unknown")
 	property.set_default_value( "123")
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property.get_default_value()).is_null()
-	
-	
+
+
 func test_resource_property() -> void:
 	var resource = load("res://splash.png")
 	var property = PandoraProperty.new("123", "property", "resource")
@@ -122,8 +122,8 @@ func test_resource_property() -> void:
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property).is_equal(property)
-	
-	
+
+
 func test_resource_property_wrong_type() -> void:
 	var resource = load("res://splash.png")
 	var property = PandoraProperty.new("123", "property", "string")
@@ -142,6 +142,70 @@ func test_array_property() -> void:
 func test_array_property_wrong_type() -> void:
 	var property = PandoraProperty.new("123", "property", "string")
 	property.set_default_value([1, 2, 3])
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property.get_default_value()).is_equal("")
+
+func test_vector2_property() -> void:
+	var vector = Vector2.ONE
+	var property = PandoraProperty.new("123", "property", "vector2")
+	property.set_default_value(vector)
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property).is_equal(property)
+
+func test_vector2_property_wrong_type() -> void:
+	var resource = Vector2.ONE
+	var property = PandoraProperty.new("123", "property", "string")
+	property.set_default_value(resource)
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property.get_default_value()).is_equal("")
+
+func test_vector2i_property() -> void:
+	var vector = Vector2i.ONE
+	var property = PandoraProperty.new("123", "property", "vector2i")
+	property.set_default_value(vector)
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property).is_equal(property)
+
+func test_vector2i_property_wrong_type() -> void:
+	var resource = Vector2i.ONE
+	var property = PandoraProperty.new("123", "property", "string")
+	property.set_default_value(resource)
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property.get_default_value()).is_equal("")
+
+func test_vector3_property() -> void:
+	var vector = Vector3.ONE
+	var property = PandoraProperty.new("123", "property", "vector3")
+	property.set_default_value(vector)
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property).is_equal(property)
+
+func test_vector3_property_wrong_type() -> void:
+	var resource = Vector3.ONE
+	var property = PandoraProperty.new("123", "property", "string")
+	property.set_default_value(resource)
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property.get_default_value()).is_equal("")
+
+func test_vector3i_property() -> void:
+	var vector = Vector3i.ONE
+	var property = PandoraProperty.new("123", "property", "vector3i")
+	property.set_default_value(vector)
+	var new_property = PandoraProperty.new("", "", "")
+	new_property.load_data(property.save_data())
+	assert_that(new_property).is_equal(property)
+
+func test_vector3i_property_wrong_type() -> void:
+	var resource = Vector3i.ONE
+	var property = PandoraProperty.new("123", "property", "string")
+	property.set_default_value(resource)
 	var new_property = PandoraProperty.new("", "", "")
 	new_property.load_data(property.save_data())
 	assert_that(new_property.get_default_value()).is_equal("")
