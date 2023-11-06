9de5493565db964c2a6a001af27e4f18e207c6ac
Nathan Krischer <me@nathankrischer.com>
2023-11-06 09:14:28 -0500

    Replaced EditorSpinSlider with SpinBox. Adjusted Enum variant names to adhere to style. Replaced boolean operators and fixed brackets.

--- addons/pandora/ui/components/properties/vector/vector_property.gd
+++ addons/pandora/ui/components/properties/vector/vector_property.gd
@@ -2,7 +2,7 @@
 extends PandoraPropertyControl
 
 
-enum VectorType { VECTOR_2, VECTOR_2i, VECTOR_3, VECTOR_3i }
+enum VectorType { VECTOR_2, VECTOR_2I, VECTOR_3, VECTOR_3I }
 
 
 const Vector2Type = preload("res://addons/pandora/model/types/vector2.gd")
@@ -17,15 +17,15 @@ const Vector3iType = preload("res://addons/pandora/model/types/vector3i.gd")
 @onready var hbox : HBoxContainer = $HBoxContainer
 
 
-var inputs : Array[EditorSpinSlider] = []
+var inputs : Array[SpinBox] = []
 
 
 func _ready() -> void:
 	var components : int
 	match vector_type:
-		VectorType.VECTOR_2, VectorType.VECTOR_2i:
+		VectorType.VECTOR_2, VectorType.VECTOR_2I:
 			components = 2
-		VectorType.VECTOR_3, VectorType.VECTOR_3i:
+		VectorType.VECTOR_3, VectorType.VECTOR_3I:
 			components = 3
 	for i in components:
 		var input := _create_editor_spin_slider(i)
@@ -48,14 +48,14 @@ func refresh() -> void:
 				min_value = _property.get_setting(Vector2Type.SETTING_MIN_COMPONENT_VALUE) as int
 				max_value = _property.get_setting(Vector2Type.SETTING_MAX_COMPONENT_VALUE) as int
 				step = _property.get_setting(Vector2Type.SETTING_STEPS) as float
-			VectorType.VECTOR_2i:
+			VectorType.VECTOR_2I:
 				min_value = _property.get_setting(Vector2iType.SETTING_MIN_COMPONENT_VALUE) as int
 				max_value = _property.get_setting(Vector2iType.SETTING_MAX_COMPONENT_VALUE) as int
 			VectorType.VECTOR_3:
 				min_value = _property.get_setting(Vector3Type.SETTING_MIN_COMPONENT_VALUE) as int
 				max_value = _property.get_setting(Vector3Type.SETTING_MAX_COMPONENT_VALUE) as int
 				step = _property.get_setting(Vector3Type.SETTING_STEPS) as float
-			VectorType.VECTOR_3i:
+			VectorType.VECTOR_3I:
 				min_value = _property.get_setting(Vector3iType.SETTING_MIN_COMPONENT_VALUE) as int
 				max_value = _property.get_setting(Vector3iType.SETTING_MAX_COMPONENT_VALUE) as int
 		for i in inputs.size():
@@ -69,41 +69,31 @@ func refresh() -> void:
 
 
 func _setting_changed(key:String) -> void:
-	if (vector_type == VectorType.VECTOR_2 &&
-		key == Vector2Type.SETTING_MIN_COMPONENT_VALUE ||
-		key == Vector2Type.SETTING_MAX_COMPONENT_VALUE ||
-		key == Vector2Type.SETTING_STEPS) \
-			|| \
-		(vector_type == VectorType.VECTOR_2i &&
-		key == Vector2iType.SETTING_MIN_COMPONENT_VALUE ||
-		key == Vector2iType.SETTING_MAX_COMPONENT_VALUE) \
-			|| \
-		(vector_type == VectorType.VECTOR_3 &&
-		key == Vector3Type.SETTING_MIN_COMPONENT_VALUE ||
-		key == Vector3Type.SETTING_MAX_COMPONENT_VALUE ||
-		key == Vector3Type.SETTING_STEPS) \
-			|| \
-		(vector_type == VectorType.VECTOR_3i &&
-		key == Vector3iType.SETTING_MIN_COMPONENT_VALUE ||
-		key == Vector3iType.SETTING_MAX_COMPONENT_VALUE):
+	if (vector_type == VectorType.VECTOR_2 and
+		(key == Vector2Type.SETTING_MIN_COMPONENT_VALUE or
+		key == Vector2Type.SETTING_MAX_COMPONENT_VALUE or
+		key == Vector2Type.SETTING_STEPS)) \
+			or \
+		(vector_type == VectorType.VECTOR_2I and
+		(key == Vector2iType.SETTING_MIN_COMPONENT_VALUE or
+		key == Vector2iType.SETTING_MAX_COMPONENT_VALUE)) \
+			or \
+		(vector_type == VectorType.VECTOR_3 and
+		(key == Vector3Type.SETTING_MIN_COMPONENT_VALUE or
+		key == Vector3Type.SETTING_MAX_COMPONENT_VALUE or
+		key == Vector3Type.SETTING_STEPS)) \
+			or \
+		(vector_type == VectorType.VECTOR_3I and
+		(key == Vector3iType.SETTING_MIN_COMPONENT_VALUE or
+		key == Vector3iType.SETTING_MAX_COMPONENT_VALUE)):
 			refresh()
 
 
-func _draw() -> void:
-	var sb : StyleBox = get_theme_stylebox(&"panel", &"Tree")
-	draw_style_box(sb, get_rect())
-
-
-func _create_editor_spin_slider(axis:int) -> EditorSpinSlider:
-	var node := EditorSpinSlider.new()
+func _create_editor_spin_slider(axis:int) -> SpinBox:
+	var node := SpinBox.new()
 	var label = ["x", "y", "z"][axis]
-	node.label = label
-	node.add_theme_color_override(
-		&"label_color",
-		get_theme_color(StringName("property_color_" + label), &"Editor"))
 	node.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	node.flat = true
-	node.hide_slider = true
+	node.prefix = label
 	if vector_type == VectorType.VECTOR_2 or vector_type == VectorType.VECTOR_3:
 		node.step = 0.01
 	node.focus_entered.connect(func(): focused.emit())
