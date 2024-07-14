a14bd88c883a95f9f1751d81fb72eacad02cfd52
bitbrain <miguel-gonzalez@gmx.de>
2024-07-14 14:16:35 +0100

    Do not fail if property got removed

--- addons/pandora/model/entity.gd
+++ addons/pandora/model/entity.gd
@@ -693,8 +693,9 @@ func _save_overrides() -> Dictionary:
 	for property_name in _property_overrides:
 		var value = _property_overrides[property_name]
 		var property = get_entity_property(property_name)
-		var type = property.get_property_type()
-		output[property_name] = {"type": type.get_type_name(), "value": type.write_value(value)}
+		if property != null:
+			var type = property.get_property_type()
+			output[property_name] = {"type": type.get_type_name(), "value": type.write_value(value)}
 	return output
 
 
