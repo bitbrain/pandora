51f2ed70c437767b888597b4b17fc27765515410
Shomy <61943525+shomykohai@users.noreply.github.com>
2024-07-20 18:48:53 +0200

    Apply suggested changes (#197)

--- addons/pandora/model/type.gd
+++ addons/pandora/model/type.gd
@@ -74,10 +74,7 @@ static func lookup(name: String) -> PandoraPropertyType:
 	if ResourceLoader.exists(type_path):
 		var ScriptType = load(type_path)
 		if ScriptType != null:
-			if Engine.is_editor_hint() and ScriptType.has_source_code():
-				return ScriptType.new()
-		else:
-			if ScriptType.can_instantiate():
+			if ScriptType.has_source_code() or ScriptType.can_instantiate():
 				return ScriptType.new()
 
 	return UndefinedType.new()
