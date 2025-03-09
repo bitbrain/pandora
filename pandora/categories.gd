50a9d20338c3cc7c3e2823e3f8fde6a15c16fc0a
bitbrain <miguel-gonzalez@gmx.de>
2025-03-09 11:23:42 +0000

    Upgrade to Godot 4.4

--- pandora/categories.gd
+++ pandora/categories.gd
@@ -2,22 +2,11 @@
 class_name PandoraCategories
 
 
-const ITEMS = "1"
-const RARITY = "16"
-const QUESTS = "27"
-const DIALOGUES = "28"
-const SPELLS = "29"
-const MOCK__REQUIRED_FOR_TESTING__ = "54"
-const NPCS = "2"
+const ROOT = "BfNTMLO-WG"
 
 
-class ItemsArmoryCategories:
-	const SHIELDS = "8"
-
-
-class ItemsCategories:
-	const ARMORY = "3"
-	const TOOLS = "4"
-	const ORES = "13"
+class RootCategories:
+	const ITEMS = "t2gVU9hBys"
+	const NPCS = "dH4s0ttwht"
 
 
